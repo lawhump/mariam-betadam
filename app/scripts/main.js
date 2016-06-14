be818f9a466e29331abdc675eb399195ce1fdf60
Lawrence Humphrey <lawrenceh1993@gmail.com>
2016-06-13 20:08:11 -0500

    this code belongs in a garbage can

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,72 +1,35 @@
 'use strict';
-
 var container = document.getElementById('placeholder');
 var destination = location.hash;
 
+var init = (function () {
+  var api = {};
+
+  api.route = function() {
+    destination = location.hash;
+    if (destination.includes('project')) {
+      var url = window.location.hash.split('project/')[1];
+      url = 'markup/' + url + '.html';
+      getMarkup(url, responseHandler);
+    }
+
+    else {
+      getMarkup('markup/projects.html', responseHandler);
+    }
+  };
+
+  return api;
+})();
+
 var fadeContentIn = function() {
-  container.firstChild.className += ' fade-in';
+  container.firstChild.classList.add('fade-in');
 }
 
-var responseProjsHandler = function(res) {
-  // TODO Error checking
-  container.innerHTML = res;
-  initializeGrid();
-  fadeContentIn();
-};
-
-var responseProjHandler = function(res) {
+var responseHandler = function(res) {
   // TODO Error checking
   container.innerHTML = res;
   fadeContentIn();
 };
-
-var responseWorkHandler = function(res) {
-  console.log(res);
-  // TODO Error checking
-  container.innerHTML = res;
-  fadeContentIn();
-};
-
-var responseContHandler = function(res) {
-  console.log(res);
-  // TODO Error checking
-  container.innerHTML = res;
-  fadeContentIn();
-};
-
-var route = function() {
-  destination = location.hash;
-  console.log(destination);
-  if (destination == '#work') {
-    getMarkup('work.html', responseWorkHandler);
-  }
-
-  else if (destination.length == 0) {
-    getMarkup('projects.html', responseProjsHandler);
-  }
-
-  else if (destination.includes('project')) {
-    getMarkup('project.html', responseProjHandler);
-  }
-
-  else {
-    return;
-  }
-};
-
-
-function initializeGrid() {
-  // var grid = document.querySelector('.projects');
-  // var options = {
-  //   itemSelector: '.project',
-  //   columnWidth: '.grid-sizer',
-  //   isFitWidth: true,
-  //   gutter: 10
-  // };
-  // var msnry = new Masonry(grid, options);
-
-  document.querySelector('.projects').addEventListener('click', clickHandler);
-}
 
 function getMarkup(url, callback) {
   var xmlHttp = new XMLHttpRequest();
@@ -80,17 +43,21 @@ function getMarkup(url, callback) {
 }
 
 function clickHandler(e) {
+  console.log('wtf');
   if(e.target && e.target.nodeName == 'DIV') {
-		// List item found!  Output the ID!
     var projectName = e.target.dataset.projectName;
     console.log(projectName);
-    document.querySelector('.projects').className += 'fade-out';
+    document.querySelector('.projects').classList.add('fade-out');
     window.location.hash = '#project/' + projectName;
 	}
 }
 
-window.onhashchange = route;
 
 (function() {
-  route();
+  var app = init;
+  app.route();
+
+  window.onhashchange = app.route;
 })();
+
+document.querySelector('#placeholder').addEventListener('click', clickHandler);
