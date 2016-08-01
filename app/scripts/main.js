e6b8bd9c68d392867f7f7c15f5c19e3bf4c21433
Lawrence Humphrey <lawrenceh1993@gmail.com>
2016-08-01 17:40:42 -0500

    smooth scrolling

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -18,6 +18,101 @@ var thirdRow = document.querySelectorAll('.project')[8];
 var secondRowThreshhold = secondRow.scrollHeight - secondRow.clientHeight + 10;
 var thirdRowThreshhold = thirdRow.scrollHeight - thirdRow.clientHeight + 10;
 
+/**
+ *
+ * Created by Borbás Geri on 12/17/13
+ * Copyright (c) 2013 eppz! development, LLC.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+var EPPZScrollTo = {
+  /**
+   * Helpers.
+   */
+  documentVerticalScrollPosition: function()
+  {
+      if (self.pageYOffset) return self.pageYOffset; // Firefox, Chrome, Opera, Safari.
+      if (document.documentElement && document.documentElement.scrollTop) return document.documentElement.scrollTop; // Internet Explorer 6 (standards mode).
+      if (document.body.scrollTop) return document.body.scrollTop; // Internet Explorer 6, 7 and 8.
+      return 0; // None of the above.
+  },
+
+  viewportHeight: function()
+  { return (document.compatMode === "CSS1Compat") ? document.documentElement.clientHeight : document.body.clientHeight; },
+
+  documentHeight: function()
+  { return (document.height !== undefined) ? document.height : document.body.offsetHeight; },
+
+  documentMaximumScrollPosition: function()
+  { return this.documentHeight() - this.viewportHeight(); },
+
+  elementVerticalClientPositionById: function(id)
+  {
+      var element = document.getElementById(id);
+      var rectangle = element.getBoundingClientRect();
+      return rectangle.top;
+  },
+
+  /**
+   * Animation tick.
+   */
+  scrollVerticalTickToPosition: function(currentPosition, targetPosition)
+  {
+      var filter = 0.2;
+      var fps = 120;
+      var duration = 1250;
+
+      var difference = parseFloat(targetPosition) - parseFloat(currentPosition);
+
+      // Snap, then stop if arrived.
+      var arrived = (Math.abs(difference) <= 0.5);
+      if (arrived)
+      {
+          // Apply target.
+          scrollTo(0.0, targetPosition);
+          return;
+      }
+
+      // Filtered position.
+      currentPosition = (parseFloat(currentPosition) * (1.0 - filter)) + (parseFloat(targetPosition) * filter);
+
+      // Apply target.
+      scrollTo(0.0, Math.round(currentPosition));
+
+      // Schedule next tick.
+      setTimeout("EPPZScrollTo.scrollVerticalTickToPosition("+currentPosition+", "+targetPosition+")", (duration / fps));
+  },
+
+  /**
+   * For public use.
+   *
+   * @param id The id of the element to scroll to.
+   * @param padding Top padding to apply above element.
+   */
+  scrollVerticalToElementById: function(id, padding)
+  {
+      var element = document.getElementById(id);
+      if (element == null)
+      {
+          console.warn('Cannot find element with id \''+id+'\'.');
+          return;
+      }
+
+      var targetPosition = this.documentVerticalScrollPosition() + this.elementVerticalClientPositionById(id) - padding;
+      var currentPosition = this.documentVerticalScrollPosition();
+
+      // Clamp.
+      var maximumScrollPosition = this.documentMaximumScrollPosition();
+      if (targetPosition > maximumScrollPosition) targetPosition = maximumScrollPosition;
+
+      // Start animation.
+      this.scrollVerticalTickToPosition(currentPosition, targetPosition);
+  }
+};
+
 var init = (function () {
   var api = {};
 
@@ -43,7 +138,7 @@ var init = (function () {
   };
 
   api.responseHandler = function(res) {
-    window.scrollTo(0,0);
+    // window.scrollTo(0,0);
     placeholder.removeAttribute('hidden');
     if (placeholder.classList.contains('active')) {
       console.log('contains active');
@@ -84,9 +179,12 @@ var init = (function () {
       var active = document.querySelector('nav .active');
       if (api.isValidPath()) {
         var path = 'markup' + api.destination.split('#')[1] + '.html';
-        api.getMarkup(path, api.responseHandler);
-        active.classList.remove('active');
-        document.querySelector('.dropdown-wrapper').classList.add('active');
+        EPPZScrollTo.scrollVerticalToElementById('nav', 100);
+        window.setTimeout(function() {
+          api.getMarkup(path, api.responseHandler);
+          active.classList.remove('active');
+          document.querySelector('.dropdown-wrapper').classList.add('active');
+        }, 400);
       }
 
       else {
